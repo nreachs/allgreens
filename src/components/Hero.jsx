494f97779545084bf959e9250de0be220b354fb2
nreachs <nreachs007@gmail.com>
2025-04-22 22:35:58 +0200

    third commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
-        className="lg:mt-6 text-sm mb-4 font-medium tracking-tight bg-gradient-to-r from-[#3d5f4e] via-[#96f4b5] to-[#3d5f4e] bg-clip-text text-transparent">
+        className="lg:mt-6 text-center px-2 text-sm mb-4 font-medium tracking-tight bg-gradient-to-r from-[#3d5f4e] via-[#96f4b5] to-[#3d5f4e] bg-clip-text text-transparent">
             {HERO_CONTENT.subtitle}
         </motion.p>
         <img src={HERO_CONTENT.image} className="w-full h-[65vh] object-cover rounded-2xl p-2" alt="hero"/>
